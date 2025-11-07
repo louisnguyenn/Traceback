f510e9b8a84d28559c36878b79bfe22ca23078b1
Louis Nguyen <louis.nguyen550@gmail.com>
2025-11-07 08:43:05 -0500

    feat: add landing page

---
+++ app/page.js
@@ -0,0 +1,36 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import Features from '../components/Features';
+import Footer from '../components/Footer';
+import Hero from '../components/Hero';
+import Navbar from '../components/Navbar';
+
+function App() {
+	const [scrolled, setScrolled] = useState(false);
+
+	useEffect(() => {
+		function handleScroll() {
+			if (window.scrollY > 50) {
+				setScrolled(true);
+			} else {
+				setScrolled(false);
+			}
+		}
+
+		window.addEventListener('scroll', handleScroll);
+
+		return () => window.removeEventListener('scroll', handleScroll);
+	}, []);
+
+	return (
+		<div className="min-h-screen bg-slate-950 text-white overflow-hidden">
+			<Navbar scrolled={scrolled} />
+			<Hero />
+			<Features />
+			<Footer />
+		</div>
+	);
+}
+
+export default App;
