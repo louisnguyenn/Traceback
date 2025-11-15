8c1efa8102018046611bc71c4ad6eca68f62ebc1
Louis Nguyen <louis.nguyen550@gmail.com>
2025-11-15 10:48:21 -0500

    feat(auth): informed user that signing up/in is loading

--- components/forms/SignUpForm.jsx
+++ components/forms/SignUpForm.jsx
@@ -120,11 +120,11 @@ export function SignupForm() {
                     d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
                   ></path>
                 </svg>
-                <span>Signing in...</span>
+                <span>Signing up...</span>
               </>
             ) : (
               <>
-                <span>Sign un with Google</span>
+                <span>Sign up with Google</span>
                 <FaGoogle size={16} />
               </>
             )}
@@ -158,11 +158,11 @@ export function SignupForm() {
                     d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
                   ></path>
                 </svg>
-                <span>Signing in...</span>
+                <span>Signing up...</span>
               </>
             ) : (
               <>
-                <span>Sign un with GitHub</span>
+                <span>Sign up with GitHub</span>
                 <Github size={16} />
               </>
             )}
