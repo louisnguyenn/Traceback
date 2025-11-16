4ac671d0a017c461bdeb9be0be7ed8419798e1f3
Louis Nguyen <louis.nguyen550@gmail.com>
2025-11-16 13:13:23 -0500

    feat(ui): add background pattern to auth sign in/up forms

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,6 +1,11 @@
 export default function AuthLayout({ children }) {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-slate-950 text-white overflow-hidden">
+    <div
+      className="flex flex-col items-center justify-center min-h-screen bg-slate-950 text-white overflow-hidden"
+      style={{
+        backgroundImage: "url('/pattern.png')",
+      }}
+    >
       {children}
     </div>
   );
