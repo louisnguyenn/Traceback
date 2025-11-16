93d49a411df2afcc78defe948ebd98f47f374b94
Louis Nguyen <louis.nguyen550@gmail.com>
2025-11-16 09:51:44 -0500

    feat(sidebar): implement sidebar

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,16 +1,14 @@
 import { AppSidebar } from '@/components/AppSidebar';
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 
-// TODO: implement sidebar in dashboard
-
 export default function DashboardLayout({ children }) {
   return (
-    <>
-      <SidebarProvider>
-        <AppSidebar>
-          <main>{children}</main>
-        </AppSidebar>
-      </SidebarProvider>
-    </>
+    <SidebarProvider>
+      <AppSidebar />
+      <main className="w-full">
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
   );
 }
