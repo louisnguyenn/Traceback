02c98fbd704b01c36a4ccaf3a10362c0afd8ff87
Louis Nguyen <louis.nguyen550@gmail.com>
2025-11-23 14:02:55 -0500

    feat(projects): inform user to create a project if no project exists

--- app/dashboard/projects/page.jsx
+++ app/dashboard/projects/page.jsx
@@ -1,5 +1,14 @@
+import { Button } from '@/components/ui/button';
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from '@/components/ui/empty';
 import { Input } from '@/components/ui/input';
-import { Search } from 'lucide-react';
+import { ArrowUpRightIcon, Folder, Search } from 'lucide-react';
 import React from 'react';
 
 const page = () => {
@@ -26,6 +35,35 @@ const page = () => {
             />
           </div>
         </div>
+
+        <Empty>
+          <EmptyHeader>
+            <EmptyMedia variant="icon">
+              <Folder />
+            </EmptyMedia>
+            <EmptyTitle>No Projects Yet</EmptyTitle>
+            <EmptyDescription>
+              You haven&apos;t created any projects yet. Get started by creating
+              your first project.
+            </EmptyDescription>
+          </EmptyHeader>
+          <EmptyContent>
+            <div className="flex gap-2">
+              <Button>Create Project</Button>
+              <Button variant="outline">Import Project</Button>
+            </div>
+          </EmptyContent>
+          <Button
+            variant="link"
+            asChild
+            className="text-muted-foreground"
+            size="sm"
+          >
+            <a href="#">
+              Learn More <ArrowUpRightIcon />
+            </a>
+          </Button>
+        </Empty>
       </main>
     </div>
   );
