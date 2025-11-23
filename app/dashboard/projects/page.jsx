cff257f29968bde4f3893e07e08d0e4fc789e0c2
Louis Nguyen <louis.nguyen550@gmail.com>
2025-11-23 12:43:00 -0500

    feat(projects): add header to projects page

--- app/dashboard/projects/page.jsx
+++ app/dashboard/projects/page.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="bg-slate-950 min-h-screen">
+      <main>
+        <header className="border-b border-slate-800 bg-slate-900/50 backdrop-blur-sm">
+          <div className="px-6 py-7">
+            <div className="flex justify-between items-center">
+              <div>
+                <h2 className="text-4xl font-bold text-white">My Projects</h2>
+              </div>
+            </div>
+          </div>
+        </header>
+      </main>
+    </div>
+  );
+};
 
-export default page
+export default page;
