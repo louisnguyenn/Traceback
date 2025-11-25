925ff7d8ce9fcd492dff1da17435e4e40fbab25a
Louis Nguyen <louis.nguyen550@gmail.com>
2025-11-24 22:38:45 -0500

    feat(prompts): update index to import branches and diffs prompts

--- prompts/index.js
+++ prompts/index.js
@@ -1,1 +1,4 @@
 export { default as onboardingPrompt } from './onboarding.prompt';
+export { default as summarizeBranchesPrompt } from './summarizeBranches.prompt';
+export { default as summarizeCommitsPrompt } from './summarizeCommits.prompt';
+export { default as summarizeDiffsPrompt } from './summarizeDiffs.prompt';
