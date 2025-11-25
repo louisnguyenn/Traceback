8d3c83ee5d5841e65bc9f587170691b16b6046cc
Louis Nguyen <louis.nguyen550@gmail.com>
2025-11-24 22:00:04 -0500

    feat(api): wrap function in try-catch block

--- services/geminiService.js
+++ services/geminiService.js
@@ -3,12 +3,18 @@ import { GoogleGenAI } from '@google/genai';
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 export async function fetchGemini(prompt) {
-  const response = await ai.models.generateContent({
-    model: 'gemini-2.5-flash',
-    contents: prompt,
-  });
-  console.log(response.text);
+  try {
+    // fetch gemini api and wait for reponse
+    const response = await ai.models.generateContent({
+      model: 'gemini-2.5-flash',
+      contents: prompt,
+    });
+    console.log(response.text);
 
-  // return the generated text
-  return response.text;
+    // return the generated text
+    return response.text;
+  } catch (error) {
+    console.error('Gemini API Error:', error.reponse?.data || error.message);
+    throw new Error('Failed to generate text');
+  }
 }
