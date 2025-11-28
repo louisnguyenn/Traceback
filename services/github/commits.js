a9b45533867dbf00ce09a2ec644c096ac0ca4743
Louis Nguyen <louis.nguyen550@gmail.com>
2025-11-28 15:15:13 -0500

    refactor(github): fix variable names

--- services/github/commits.js
+++ services/github/commits.js
@@ -1,8 +1,8 @@
-export async function fetchGitHubCommits(repo) {
-  const url =
+export async function fetchGitHubCommits(repoUrl) {
+  const apiUrl =
     repoUrl.replace('github.com', 'api.github.com/repos') + '/commits';
 
-  const res = await fetch(url, {
+  const res = await fetch(apiUrl, {
     headers: {
       Accept: 'applications/vnd.github+json',
     },
