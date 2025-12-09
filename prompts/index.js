7005aaab09ae9e18f2cff750e59c0b653cba4ba1
Louis Nguyen <louis.nguyen550@gmail.com>
2025-12-08 23:11:36 -0500

    chore: remove unused prompt exports

--- prompts/index.js
+++ prompts/index.js
@@ -1,5 +1,1 @@
 export { default as onboardingPrompt } from './onboarding.prompt';
-export { default as summarizeBranchesPrompt } from './summarizeBranches.prompt';
-export { default as summarizeCommitsPrompt } from './summarizeCommits.prompt';
-export { default as summarizeDiffsPrompt } from './summarizeDiffs.prompt';
-export { default as summarizeMergesPrompt } from './summarizeMerges.prompt';
