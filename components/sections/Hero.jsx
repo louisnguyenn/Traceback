15f62c81d8bd25d4b8db4a276dcbd6991b815370
Louis Nguyen <louis.nguyen550@gmail.com>
2025-12-15 16:53:27 -0500

    feat: remove arrow right in button

--- components/sections/Hero.jsx
+++ components/sections/Hero.jsx
@@ -1,4 +1,3 @@
-import { ArrowRight } from 'lucide-react';
 import Link from 'next/link';
 import AnimatedContent from '../animations/AnimatedContent';
 import FadeContent from '../animations/FadeContent';
@@ -81,8 +80,7 @@ const Hero = () => {
               href="/signin"
               className="group w-full sm:w-auto px-6 sm:px-8 py-3 sm:py-4 bg-linear-to-b from-blue-600 to-blue-400 rounded-lg font-semibold text-sm sm:text-base transition-all duration-300 hover:scale-105 cursor-pointer flex items-center justify-center gap-2"
             >
-              Get Started
-              <ArrowRight className="w-4 h-4 sm:h-5 group-hover:translate-x-1 transition-transform duration-300" />
+              Get Started Now
             </Link>
           </FadeContent>
         </div>
